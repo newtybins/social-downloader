6fd2ece0603e84283f7eb167bffeb1ce3eed78c4
newtykins <newtykins@outlook.com>
2022-02-14 22:21:59 +0000

    feat: complete rewrite of twitdown + save dialogue

--- src/background.js
+++ src/background.js
@@ -1,35 +1,42 @@
-"use strict";
+'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+const fetchVideoUrl = url => {
+    return new Promise((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('url', url);
 
-const regex = /http[s]?:\/\/twitter.com\/.*\/status\/[0-9]*/gi;
+        fetch('https://www.savetweetvid.com/downloader', {
+            method: 'post',
+            body: formData
+        })
+            .then(res => res.text())
+            .then(res => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(res, 'text/html');
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  if (regex.test(tab.url)) {
-    const formData = new FormData();
-    formData.append("url", tab.url);
+                const [button] = doc.getElementsByClassName('btn btn-download');
 
-    fetch("https://www.savetweetvid.com/downloader", {
-      method: "post",
-      body: formData,
-    })
-      .then((res) => res.text())
-      .then((res) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(res, "text/html");
-        const button = doc.getElementsByClassName("btn btn-download")[0];
+                if (button && button.href) {
+                    resolve(button.href);
+                } else {
+                    reject();
+                }
+            });
+    });
+};
 
-        chrome.tabs.create({ url: button.href });
-      })
-      .catch(() => {
-        alert(
-          "There was no video or GIF on this page! If this is an error, please open an issue at newtykins/twitdown on GitHub!"
+chrome.browserAction.onClicked.addListener(({ url }) => {
+    fetchVideoUrl(url)
+        .then(async video => {
+            // todo: Add an option to instead open in the next tab
+            chrome.downloads.download({
+                url: video,
+                saveAs: true
+            });
+        })
+        .catch(e =>
+            chrome.tabs.executeScript({
+                code: "alert('A Twitter video could not be detected, or there was an error parsing your request!');"
+            })
         );
-      });
-  } else {
-    alert("This is not a valid tweet URL!");
-  }
 });
